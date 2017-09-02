1abde19dc09a5e9ae4226846b0a6cc9a0c8188eb
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-09-02 23:31:52 +0800

    pretty code, add fill outlier with mean

--- experiment_wenfeng/feature_outlier_utils.py
+++ experiment_wenfeng/feature_outlier_utils.py
@@ -16,32 +16,18 @@ def get_series_percentile(pd_series, lpercentile=0.5, upercentile=99.5):
 def get_series_q1q3(pd_series):
     return get_series_percentile(pd_series, 25, 75)
 
-def get_feature_outlier_index(pd_series, method):
-    """
-    :param pd_series:
-    :param method:
-    :return: index list of outliers
-    """
-    methods = ['inter_quartile_range', 'small_probability_event']
-    assert method in methods
+def replace_with_iqr_value(pd_series, q1, q3, value):
+    iqr = q3 - q1
+    llimit = q1 - 1.5 * iqr
+    ulimit = q3 + 1.5 * iqr
 
-    ret = None
-    series = pd_series[pd_series.notnull()]
+    return replace_with_value(pd_series, llimit, ulimit, value)
 
-    if method == methods[0]:
-        q1, q3 = get_series_q1q3(pd_series)
-        iqr = q3 - q1
-        ret = [i for i, v in zip(series.index, series.values) if v < q1-1.5*iqr or v > q3+1.5*iqr]
-
-    elif method == methods[1]:
-        llimit, ulimit = get_series_percentile(pd_series, 0.5, 99.5)
-        ret = [i for i, v in zip(series.index, series.values) if v > ulimit or v < llimit]
-
-    return ret
-
-def replace_feature_outlier_value(pd_series, indexs, value):
+def replace_with_value(pd_series, llimit, ulimit, value):
     new_series = pd_series.copy()
-    new_series[indexs] = value
+
+    new_series.loc[new_series > ulimit] = value
+    new_series.loc[new_series < llimit] = value
 
     return new_series
 
@@ -63,32 +49,18 @@ def replace_with_boundary(pd_series, llimit, ulimit):
 def generate_feature_replace_outlier(pd_series):
     ret = OrderedDict()
 
-    outlier_indexs_iqr = get_feature_outlier_index(pd_series, 'inter_quartile_range')
-    outlier_indexs_spe = get_feature_outlier_index(pd_series, 'small_probability_event')
-
     q1, q3 = get_series_q1q3(pd_series)
     llimit, ulimit = get_series_percentile(pd_series)
 
-    print 'Feature Outlier: len(outlier_indexs_iqr) = %d, len(outlier_indexs_spe) = %d' % (
-        len(outlier_indexs_iqr), len(outlier_indexs_spe))
+    ret['iqr_nan']      = replace_with_iqr_value(pd_series, q1, q3, np.NAN)
+    ret['iqr_median']   = replace_with_iqr_value(pd_series, q1, q3, pd_series.median())
+    ret['iqr_mean']     = replace_with_iqr_value(pd_series, q1, q3, pd_series.mean())
+    ret['iqr_boundary'] = replace_with_iqr_boundary(pd_series, q1, q3)
 
-    ret['iqr_nan'] = \
-        replace_feature_outlier_value(pd_series, outlier_indexs_iqr, np.NAN)
-
-    ret['iqr_median'] = \
-        replace_feature_outlier_value(pd_series, outlier_indexs_iqr, pd_series.median())
-
-    ret['iqr_boundary'] = \
-        replace_with_iqr_boundary(pd_series, q1, q3)
-
-    ret['spe_nan'] = \
-        replace_feature_outlier_value(pd_series, outlier_indexs_spe, np.NAN)
-
-    ret['spe_median'] = \
-        replace_feature_outlier_value(pd_series, outlier_indexs_spe, pd_series.median())
-
-    ret['spe_boundary'] = \
-        replace_with_boundary(pd_series, llimit, ulimit)
+    ret['spe_nan']      = replace_with_value(pd_series, llimit, ulimit, np.NAN)
+    ret['spe_median']   = replace_with_value(pd_series, llimit, ulimit, pd_series.median())
+    ret['spe_mean']     = replace_with_value(pd_series, llimit, ulimit, pd_series.mean())
+    ret['spe_boundary'] = replace_with_boundary(pd_series, llimit, ulimit)
 
     return ret
 
@@ -96,14 +68,6 @@ def generate_feature_replace_outlier(pd_series):
 if __name__ == '__main__':
     numbers = np.array([np.NAN, 1, 4, 5, 6, 7, 8, 9, 10, 11, 11, 12, np.NAN, 22])
     numbers_series = pd.Series(numbers)
-
-    outlier_indexs_iqr = get_feature_outlier_index(numbers_series, 'inter_quartile_range')
-    outlier_indexs_spe = get_feature_outlier_index(numbers_series, 'small_probability_event')
-    print 'outlier_indexs_iqr', outlier_indexs_iqr
-    print 'outlier_indexs_spe', outlier_indexs_spe
-
-    # new_numbers_series = replace_feature_outlier_value(numbers_series, outlier_indexs_spe, np.NaN)
-    # print numbers_series, new_numbers_series
 
     for name, series in generate_feature_replace_outlier(numbers_series).items():
         print name
