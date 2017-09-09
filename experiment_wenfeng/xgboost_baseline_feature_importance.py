2c9881f55a1b935878841eec9b4f1fdcb32a6ff8
Yang Wenfeng <sysuyangwf@gmail.com>
2017-09-09 23:25:17 +0800

    add feature importance test result

--- experiment_wenfeng/xgboost_baseline_feature_importance.py
+++ experiment_wenfeng/xgboost_baseline_feature_importance.py
@@ -2,6 +2,26 @@
 #
 # Training result: [192] train-mae:0.051412 holdout-mae:0.051941
 # Public score: 0.0646266
+# Drop missing rate >= 0.9950 features:
+# [buildingclasstypeid,finishedsquarefeet13,basementsqft,
+# storytypeid,yardbuildingsqft26,architecturalstyletypeid,
+# typeconstructiontypeid,finishedsquarefeet6]
+# Training result: [178] train-mae:0.051431 holdout-mae:0.052045
+# Public score: 0.0646217 improve 0.68% = (0.0646266 - 0.0646217)/(0.0646266 - 0.0639094)
+# Drop missing rate >= 0.9500 features:
+# [buildingclasstypeid,finishedsquarefeet13,basementsqft,
+# storytypeid,yardbuildingsqft26,architecturalstyletypeid,
+# typeconstructiontypeid,finishedsquarefeet6,decktypeid,
+# poolsizesum,pooltypeid10,pooltypeid2,taxdelinquencyyear,
+# yardbuildingsqft17,finishedsquarefeet15]
+# Training result: [181] train-mae:0.051411 holdout-mae:0.051993
+# Public score: 0.0646936
+# Drop zero variance features:
+# [buildingclasstypeid,decktypeid,poolcnt,pooltypeid10,
+# pooltypeid2,pooltypeid7,storytypeid,assessmentyear]
+# Training result: [178] train-mae:0.051562 holdout-mae:0.052138
+# Public score: 0.0647126
+
 import common_utils as cu
 import xgboost as xgb
 
@@ -57,10 +77,16 @@ def run_missing_rate():
 
     print '\n'.join(','.join(str(o) for o in one) for one in result)
 
-
-def run():
+def run_zero_variance():
     # read train data.
     X, y = cu.get_train_data(encode_non_object=False)
+
+    # feature utils
+    from feature_utils import get_zero_variance_features
+    columns = get_zero_variance_features()
+
+    print 'Drop zero variance features [%s]' % ','.join(columns)
+    X = X.drop(columns, axis=1)
 
     # get CV from train data.
     X_train, y_train, X_holdout, y_holdout = cu.get_cv(X, y)
@@ -69,17 +95,47 @@ def run():
     xgbm = XGBoostModel()
     xgbm.train(X_train, y_train, X_holdout, y_holdout)
 
-    # # read test data.
-    # T = cu.get_test_data(encode_non_object=False)
-    #
-    # # predict result.
-    # print('Predicting.')
-    # y_pred = xgbm.predict(T[X_train.columns])
-    #
-    # # write result.
-    # cu.write_result(y_pred)
+    # read test data.
+    T = cu.get_test_data(encode_non_object=False)
+
+    # predict result.
+    print('Predicting.')
+    y_pred = xgbm.predict(T[X_train.columns])
+
+    # write result.
+    cu.write_result(y_pred)
+
+def run():
+    # read train data.
+    X, y = cu.get_train_data(encode_non_object=False)
+
+    # feature utils
+    from feature_utils import get_feature_missing_df, get_features_by_missing_rate
+    missing_df = get_feature_missing_df(X)
+    missing_rate = 0.995
+    columns = get_features_by_missing_rate(missing_df, missing_rate)
+    print "Drop missing rate >= %.4f features: [%s]" % (missing_rate, ','.join(columns))
+    X = X.drop(columns, axis=1)
+
+    # get CV from train data.
+    X_train, y_train, X_holdout, y_holdout = cu.get_cv(X, y)
+
+    # train model.
+    xgbm = XGBoostModel()
+    xgbm.train(X_train, y_train, X_holdout, y_holdout)
+
+    # read test data.
+    T = cu.get_test_data(encode_non_object=False)
+
+    # predict result.
+    print('Predicting.')
+    y_pred = xgbm.predict(T[X_train.columns])
+
+    # write result.
+    cu.write_result(y_pred)
 
 
 if __name__ == "__main__":
-    # run()
-    run_missing_rate()
+    run()
+    # run_missing_rate()
+    # run_zero_variance()
