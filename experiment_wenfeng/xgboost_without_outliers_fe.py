cc300c19899be1d806571d340118eb8690dd8d6c
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-09-19 21:55:58 +0800

    fe

--- experiment_wenfeng/xgboost_without_outliers_fe.py
+++ experiment_wenfeng/xgboost_without_outliers_fe.py
@@ -212,45 +212,45 @@ def explore_feature_interaction():
     result = []
 
     print 'add_feature_missing_count'
-    x_train, df_test = FeatureInteraction.add_feature_missing_count(x_train.copy(), df_test.copy())
-    best_score, _ = xgboost_cross_validation(x_train, y_train)
+    x_train_new, _ = FeatureInteraction.add_feature_missing_count(x_train.copy(), df_test.copy())
+    best_score, _ = xgboost_cross_validation(x_train_new, y_train)
     result.append(['add_feature_missing_count', best_score])
     print 'result', ','.join(str(e) for e in result[-1])
 
     print 'add_regionidzip_centroid_distance'
-    x_train, df_test = FeatureInteraction.add_regionidzip_centroid_distance(x_train.copy(), df_test.copy())
-    best_score, _ = xgboost_cross_validation(x_train, y_train)
+    x_train_new, _ = FeatureInteraction.add_regionidzip_centroid_distance(x_train.copy(), df_test.copy())
+    best_score, _ = xgboost_cross_validation(x_train_new, y_train)
     result.append(['regionidzip_centroid_distance', best_score])
     print 'result', ','.join(str(e) for e in result[-1])
 
     print 'add_feature_division'
     for col1 in ['structuretaxvaluedollarcnt', 'taxamount', 'landtaxvaluedollarcnt']:
         for col2 in ['calculatedfinishedsquarefeet', 'lotsizesquarefeet']:
-            x_train, df_test = FeatureInteraction.add_feature_division(x_train.copy(), df_test.copy(), col1, col2)
-            best_score, _ = xgboost_cross_validation(x_train, y_train)
+            x_train_new, _ = FeatureInteraction.add_feature_division(x_train.copy(), df_test.copy(), col1, col2)
+            best_score, _ = xgboost_cross_validation(x_train_new, y_train)
             result.append(['add_feature_division_%s_div_%s' % (col1, col2), best_score])
             print 'result', ','.join(str(e) for e in result[-1])
 
     print 'add_feature_mod'
     for col in ['structuretaxvaluedollarcnt', 'landtaxvaluedollarcnt', 'taxvaluedollarcnt', 'taxamount']:
         for num in [10, 100, 1000, 10000]:
-            x_train, df_test = FeatureInteraction.add_feature_mod(x_train.copy(), df_test.copy(), col, num)
-            best_score, _ = xgboost_cross_validation(x_train, y_train)
+            x_train_new, _ = FeatureInteraction.add_feature_mod(x_train.copy(), df_test.copy(), col, num)
+            best_score, _ = xgboost_cross_validation(x_train_new, y_train)
             result.append(['add_feature_mod_%s_mod_%d' % (col, num), best_score])
             print 'result', ','.join(str(e) for e in result[-1])
 
     for col in ['lotsizesquarefeet', 'calculatedfinishedsquarefeet', 'finishedsquarefeet12']:
         for num in [10, 20, 50, 100]:
-            x_train, df_test = FeatureInteraction.add_feature_mod(x_train.copy(), df_test.copy(), col, num)
-            best_score, _ = xgboost_cross_validation(x_train, y_train)
+            x_train_new, _ = FeatureInteraction.add_feature_mod(x_train.copy(), df_test.copy(), col, num)
+            best_score, _ = xgboost_cross_validation(x_train_new, y_train)
             result.append(['add_feature_mod_%s_mod_%d' % (col, num), best_score])
             print 'result', ','.join(str(e) for e in result[-1])
 
     print 'add_feature_around'
     for decimals in [0, 1, 2, 3, 4, 5, 6, 7]:
-        x_train, df_test = FeatureInteraction.add_feature_around(x_train.copy(), df_test.copy(), 'latitude', decimals)
-        x_train, df_test = FeatureInteraction.add_feature_around(x_train, df_test, 'longitude', decimals)
-        best_score, _ = xgboost_cross_validation(x_train, y_train)
+        x_train_new, tmp = FeatureInteraction.add_feature_around(x_train.copy(), df_test.copy(), 'latitude', decimals)
+        x_train_new, _ = FeatureInteraction.add_feature_around(x_train_new, tmp, 'longitude', decimals)
+        best_score, _ = xgboost_cross_validation(x_train_new, y_train)
         result.append(['add_feature_%s_around_%d' % ('latitudelongitude', decimals), best_score])
         print 'result', ','.join(str(e) for e in result[-1])
 
@@ -258,10 +258,30 @@ def explore_feature_interaction():
     for col in ['structuretaxvaluedollarcnt', 'landtaxvaluedollarcnt', 'taxvaluedollarcnt', 'taxamount',
                 'lotsizesquarefeet', 'calculatedfinishedsquarefeet', 'finishedsquarefeet12']:
         for num in [10, 100, 1000, 10000]:
-            x_train, df_test = FeatureInteraction.add_feature_log(x_train.copy(), df_test.copy(), col)
-            best_score, _ = xgboost_cross_validation(x_train, y_train)
+            x_train_new, _ = FeatureInteraction.add_feature_log(x_train.copy(), df_test.copy(), col)
+            best_score, _ = xgboost_cross_validation(x_train_new, y_train)
             result.append(['add_feature_mod_%s_mod_%d' % (col, num), best_score])
             print 'result', ','.join(str(e) for e in result[-1])
+
+    print 'LabelCountEncoder'
+    for col in ['airconditioningtypeid', 'architecturalstyletypeid',
+                'buildingclasstypeid', 'buildingqualitytypeid', 'decktypeid',
+                'heatingorsystemtypeid',
+                'propertylandusetypeid', 'storytypeid', 'typeconstructiontypeid',
+                'regionidcity', 'regionidcounty', 'regionidneighborhood',
+                'regionidzip', 'rawcensustractandblock', 'censustractandblock',
+                'fips', 'pooltypeid2', 'pooltypeid7',
+                'fips', 'pooltypeid2', 'pooltypeid7',
+                'pooltypeid10']:
+        lce = LabelCountEncoder()
+        x_train_new = x_train.copy()
+        # df_test_new = df_test.copy()
+        lce.fit(x_train_new[col])
+        x_train_new[col] = lce.transform(x_train_new[col])
+        # df_test[col] = lce.transform(df_test[col])
+        best_score, _ = xgboost_cross_validation(x_train_new, y_train)
+        result.append(['LabelCountEncoder_%s' % col, best_score])
+        print 'result', ','.join(str(e) for e in result[-1])
 
     print '\n'.join(','.join([str(e) for e in one]) for one in result)
 
@@ -325,7 +345,7 @@ if __name__ == "__main__":
         func_name = sys.argv[1]
 
     # explore_feature_scaler, explore_feature_outlier
-    # func_name = 'explore_feature_scaler'
+    # func_name = 'explore_feature_interaction'
 
     print 'Call %s(), debug is %s' % (func_name, debug)
     eval(func_name)()
