b95c8ecc94e8b31e30285a258dfad6186323b871
gzyangwenfeng <gzyangwenfeng@corp.netease.com>
2017-09-20 21:37:21 +0800

    fe

--- experiment_wenfeng/xgboost_without_outliers_fe.py
+++ experiment_wenfeng/xgboost_without_outliers_fe.py
@@ -1,9 +1,7 @@
 # Version 1: XGBoost without outlier.
 # Training result: [220] train-mae:0.0509354 test-mae:0.0526506
 # Public score: 0.0645843
-# FE OutlierEncoder taxamount/yearbuilt 0.0646074
-# FE Merge without MeanEncoder, PB
-
+# MeanEncoder PB 0.0645381
 
 import numpy as np
 import pandas as pd
@@ -12,7 +10,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import RobustScaler
 from feature_utils import LabelCountEncoder, FeatureInteraction
 from feature_utils import OutlierEncoder
-from mean_encoder import MeanEncoder
 
 OUTLIER_UPPER_BOUND = 0.419
 OUTLIER_LOWER_BOUND = -0.4
@@ -32,11 +29,6 @@ def get_xgb_params(y_train_mean):
         'silent': 1
     }
     return xgb_params
-
-def reset_nan(df, columns):
-    for col in columns:
-        df[col].replace(to_replace=-1, value=np.nan, inplace=True)
-    return df
 
 def get_train_test_data():
     print('Reading training data, properties and test data.')
@@ -104,7 +96,6 @@ def xgboost_cross_validation(x_train, y_train):
 
 def train_and_predict(x_train, y_train, df_test, num_boost_rounds, output_suffix=''):
     print('Training the model and predict.')
-    y_train_mean = y_train.mean()
 
     # xgboost params
     xgb_params = get_xgb_params(y_train.mean())
@@ -125,91 +116,6 @@ def train_and_predict(x_train, y_train, df_test, num_boost_rounds, output_suffix
     output_file = '../../data/xgboost_without_outlier{}.csv'.format(output_suffix)
     print('Writing to csv [%s].' % output_file)
     test.to_csv(output_file, index=False, float_format='%.4f')
-
-def feature_scaler(x_train, df_test):
-    print 'Feature Scaler.'
-    columns = ['taxamount', 'yearbuilt']
-    for col in columns:
-        scaler = RobustScaler()
-        x_train[col] = scaler.fit_transform(x_train[[col]])[:, 0]
-        df_test[col] = scaler.transform(df_test[[col]])[:, 0]
-
-    return x_train, df_test
-
-def feature_outlier(x_train, df_test):
-    fe_columns = ['taxamount', 'yearbuilt']
-
-    # reset nan
-    x_train = reset_nan(x_train, fe_columns)
-    df_test = reset_nan(df_test, fe_columns)
-
-    print 'Feature Outlier.'
-    outlier_encoder = OutlierEncoder(method='iqr', replace='median')
-    col = 'yearbuilt'
-    outlier_encoder.fit(x_train[col])
-    x_train[col] = outlier_encoder.transform(x_train[col])
-    df_test[col] = outlier_encoder.transform(df_test[col])
-
-    outlier_encoder = OutlierEncoder(method='spe', replace='mean')
-    col = 'taxamount'
-    outlier_encoder.fit(x_train[col])
-    x_train[col] = outlier_encoder.transform(x_train[col])
-    df_test[col] = outlier_encoder.transform(df_test[col])
-
-    # x_train.fillna(-1, inplace=True)
-    # df_test.fillna(-1, inplace=True)
-
-    return x_train, df_test
-
-def explore_feature_outlier():
-    print "Explore feature outlier."
-    x_train, y_train, df_test = get_train_test_data()
-
-    fe_columns = ['taxamount', 'yearbuilt']
-
-    # reset nan
-    x_train = reset_nan(x_train, fe_columns)
-
-    result = []
-    for col in fe_columns:
-        for method in ['iqr', 'spe']:
-            for replace in ['mean', 'median', 'boundary', 'nan']:
-                if replace == 'boundary' and method != 'iqr':
-                    continue
-                print 'col = %s, method = %s, replace = %s' % (col, method, replace)
-
-                x_train_new = x_train.copy()
-                outlier_encoder = OutlierEncoder(method=method, replace=replace)
-                outlier_encoder.fit(x_train_new[col])
-                x_train_new[col] = outlier_encoder.transform(x_train_new[col])
-
-                best_score, _ = xgboost_cross_validation(x_train_new, y_train)
-                result.append([col, method, replace, best_score])
-
-    print 'Feature Engineering with OutlierEncoder.'
-    print '\n'.join(','.join([str(e) for e in one]) for one in result)
-
-
-def explore_feature_scaler():
-    """
-    # default localCV: 0.05264420
-    # RobustScaler only with taxamount, LocalCV: 0.05264420, PB: 0.0645843
-    # RobustScaler with taxamount & yearbuilt, LocalCV: 0.05263960, PB: 0.0645880
-    """
-    print "Explore feature scaler."
-    x_train, y_train, df_test = get_train_test_data()
-    fe_columns = ['taxamount', 'yearbuilt']
-
-    result = []
-    for col in fe_columns:
-        scaler = RobustScaler()
-        x_train_new = x_train.copy()
-        x_train_new[col] = scaler.fit_transform(x_train_new[[col]])[:, 0]
-
-        best_score, _ = xgboost_cross_validation(x_train_new, y_train)
-        result.append([col, best_score])
-
-    print '\n'.join(','.join([str(e) for e in one]) for one in result)
 
 def explore_feature_interaction():
     x_train, y_train, df_test = get_train_test_data()
@@ -279,42 +185,61 @@ def explore_feature_interaction():
     #     best_score, _ = xgboost_cross_validation(x_train_new, y_train)
     #     result.append(['add_feature_exp_%s' % col, best_score])
     #     print 'result', ','.join(str(e) for e in result[-1])
+    #
+    # print 'LabelCountEncoder'
+    # for col in ['airconditioningtypeid', 'architecturalstyletypeid',
+    #             'buildingclasstypeid', 'buildingqualitytypeid', 'decktypeid',
+    #             'heatingorsystemtypeid',
+    #             'propertylandusetypeid', 'storytypeid', 'typeconstructiontypeid',
+    #             'regionidcity_pred', 'regionidcounty', 'regionidneighborhood_pred',
+    #             'regionidzip_pred', 'rawcensustractandblock', 'censustractandblock',
+    #             'fips', 'pooltypeid2', 'pooltypeid7',
+    #             'fips', 'pooltypeid2', 'pooltypeid7',
+    #             'pooltypeid10']:
+    #     lce = LabelCountEncoder()
+    #     x_train_new = x_train.copy()
+    #     # df_test_new = df_test.copy()
+    #     lce.fit(x_train_new[col])
+    #     x_train_new[col] = lce.transform(x_train_new[col])
+    #     # df_test[col] = lce.transform(df_test[col])
+    #     best_score, _ = xgboost_cross_validation(x_train_new, y_train)
+    #     result.append(['LabelCountEncoder_%s' % col, best_score])
+    #     print 'result', ','.join(str(e) for e in result[-1])
 
-    print 'LabelCountEncoder'
-    for col in ['regionidcity_pred', 'regionidcounty', 'regionidneighborhood_pred',
-                'regionidzip_pred', 'rawcensustractandblock', 'censustractandblock',
-                'fips', 'pooltypeid2', 'pooltypeid7',
-                'fips', 'pooltypeid2', 'pooltypeid7',
-                'pooltypeid10']:
-        # 'airconditioningtypeid', 'architecturalstyletypeid',
-        #         'buildingclasstypeid', 'buildingqualitytypeid', 'decktypeid',
-        #         'heatingorsystemtypeid',
-        #         'propertylandusetypeid', 'storytypeid', 'typeconstructiontypeid',
-        #
-        lce = LabelCountEncoder()
+    print 'RobustScaler'
+    for col in ['taxamount', 'yearbuilt', 'structuretaxvaluedollarcnt', 'lotsizesquarefeet',
+                'landtaxvaluedollarcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet',
+                'finishedsquarefeet12']:
+        scaler = RobustScaler()
         x_train_new = x_train.copy()
-        # df_test_new = df_test.copy()
-        lce.fit(x_train_new[col])
-        x_train_new[col] = lce.transform(x_train_new[col])
-        # df_test[col] = lce.transform(df_test[col])
+        x_train_new[col] = scaler.fit_transform(x_train_new[[col]])[:, 0]
         best_score, _ = xgboost_cross_validation(x_train_new, y_train)
-        result.append(['LabelCountEncoder_%s' % col, best_score])
+        result.append(['RobustScaler_%s' % col, best_score])
+        print 'result', ','.join(str(e) for e in result[-1])
+
+    print 'OutlierEncoder'
+    for col in ['taxamount', 'yearbuilt']:
+        for method in ['iqr', 'spe']:
+            for replace in ['mean', 'median', 'boundary', 'nan']:
+                if replace == 'boundary' and method != 'iqr':
+                    continue
+                x_train_new = x_train.copy()
+                outlier_encoder = OutlierEncoder(method=method, replace=replace)
+                outlier_encoder.fit(x_train_new[col])
+                x_train_new[col] = outlier_encoder.transform(x_train_new[col])
+                best_score, _ = xgboost_cross_validation(x_train_new, y_train)
+                result.append(['OutlierEncoder_%s_%s_%s' % (col, method, replace), best_score])
+                print 'result', ','.join(str(e) for e in result[-1])
+
+    print '2016-Year'
+    for col in ['taxdelinquencyyear']:
+        x_train_new = x_train.copy()
+        x_train_new[col] = 2016 - x_train_new[col]
+        result.append(['2016-Year_%s' % col, best_score])
         print 'result', ','.join(str(e) for e in result[-1])
 
     print '\n'.join(','.join([str(e) for e in one]) for one in result)
 
-def explore_feature_geo():
-    """
-    Local CV: 0.05264520
-    """
-    x_train, y_train, df_test = get_train_test_data()
-    x_train, df_test = FeatureInteraction.add_regionidzip_centroid_distance(x_train, df_test)
-
-    result = []
-    best_score, _ = xgboost_cross_validation(x_train, y_train)
-    result.append(['regionidzip_centroid_distance', best_score])
-
-    print '\n'.join(','.join([str(e) for e in one]) for one in result)
 
 def run_fe_merge():
     x_train, y_train, df_test = get_train_test_data()
@@ -322,21 +247,14 @@ def run_fe_merge():
     print 'do_mean_encoder'
     x_train, y_train, df_test = FeatureInteraction().do_mean_encoder(x_train, y_train, df_test)
 
-    # LabelCountEncoder
-    # LabelCountEncoder_airconditioningtypeid,0.0526216
-    for col in ['airconditioningtypeid']:
-        lce = LabelCountEncoder()
-        lce.fit(x_train[col])
-        x_train[col] = lce.transform(x_train[col])
-        df_test[col] = lce.transform(df_test[col])
-
-    # add_feature_division
-    # [230]	train-mae:0.0507164	test-mae:0.0526378
-    # test-mae-mean = 0.05263020
-    # x_train, df_test = FeatureInteraction(True).add_feature_division(
-    #     x_train, df_test, 'landtaxvaluedollarcnt', 'lotsizesquarefeet')
-    # x_train, df_test = FeatureInteraction(True).add_feature_division(
-    #     x_train, df_test, 'taxamount', 'calculatedfinishedsquarefeet')
+    # MeanEncoder + LabelCountEncoder with 'airconditioningtypeid'
+    # LabelCountEncoder_airconditioningtypeid LocalCV 0.0526216
+    # PB 0.0645524
+    # for col in ['airconditioningtypeid']:
+    #     lce = LabelCountEncoder()
+    #     lce.fit(x_train[col])
+    #     x_train[col] = lce.transform(x_train[col])
+    #     df_test[col] = lce.transform(df_test[col])
 
     best_score, num_boost_rounds = xgboost_cross_validation(
         x_train, y_train
@@ -344,31 +262,12 @@ def run_fe_merge():
 
     train_and_predict(x_train, y_train, df_test, num_boost_rounds, output_suffix='_fe_merge')
 
-def run_feature_outlier():
-    x_train, y_train, df_test = get_train_test_data()
-
-    x_train, df_test = feature_outlier(x_train, df_test)
-
-    best_score, num_boost_rounds = xgboost_cross_validation(
-        x_train, y_train
-    )
-
-    train_and_predict(x_train, y_train, df_test, num_boost_rounds, output_suffix='_fe_outlier')
-
 def run():
     x_train, y_train, df_test = get_train_test_data()
 
     best_score, num_boost_rounds = xgboost_cross_validation(
         x_train, y_train
     )
-
-    # feature_scaler_explore(x_train, y_train, xgb_params)
-    # feature_outlier_explore(x_train, y_train, xgb_params)
-    # import sys
-    # sys.exit(0)
-
-    # x_train, df_test = feature_scaler(x_train, df_test)
-    # x_train, df_test = feature_outlier(x_train, df_test)
 
     train_and_predict(x_train, y_train, df_test, num_boost_rounds, output_suffix='')
 
